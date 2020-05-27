f2b3e716c535d0edd5f094e21a02d8ad4289ff22
Илья Грунов <ilya.grunov@gmail.com>
2020-05-27 21:19:49 +0300

    Добавлены переменные и функции для управления магом.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var gulp = require("gulp");
+var server = require("browser-sync").create();
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/js/*.js").on("change", server.reload);
+});
+
+gulp.task("start", gulp.series("server"));
